92599176e1b14be28992fa3486be1268323b2f3d
Miroslav Kravec <kravec.miroslav@gmail.com>
2019-06-10 17:56:31 +0200

    Initial react-typescript-parcel boostrap
    FROM: https://dev.to/terabaud/bootstrapping-a-react-typescript-project-with-parcel-4oia

---
+++ fe/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Component } from 'react'
+import { render } from 'react-dom'
+// import './index.css'
+
+class App extends Component {
+  render() {
+    return (<h1>Hello World!</h1>)
+  }
+}
+
+render(<App />, document.getElementById('root'))
