d424377a778cc25870ca009b9d705c7359334bd1
Miroslav Kravec <kravec.miroslav@gmail.com>
2019-06-15 13:11:55 +0200

    A bit of SCSS styling

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>todo list: docker go react</h1>
+        <div className="siteHeader"><h1>todo list: docker go react</h1></div>
         <TodoList/>
       </div>
     )
