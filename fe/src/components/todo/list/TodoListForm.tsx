4e269c39b0337e811d540f52670811f72ba6538e
Miroslav Kravec <kravec.miroslav@gmail.com>
2019-06-29 15:51:00 +0200

    Implement form validation using revalidation

--- fe/src/components/todo/list/TodoListForm.tsx
+++ fe/src/components/todo/list/TodoListForm.tsx
@@ -1,80 +1,87 @@
 import * as React from "react";
-import { Component } from "react";
-import { render } from "react-dom";
+import { useState } from "react";
+import Revalidation from "revalidation";
 
-function checkAllFieldsValid(formElements) {
-  return !Array.prototype.slice
-    .call(formElements)
-    .filter(elem => elem.name.length > 0)
-    .some(field => !field.checkValidity());
-}
+const getValue = e => e.target.value;
 
-export class TodoListForm extends Component {
-  constructor() {
-    super();
+const isValid = errorList => errorList === null || errorList.length === 0;
+const isNotEmpty = val => val.trim().length > 0;
 
-    this.state = {
-      formState: "clean",
-      name: ""
-    };
-  }
-  onSubmit(event) {
-    event.preventDefault();
-
-    this.setState({ formState: "submitted" });
-
-    const allFieldsValid = checkAllFieldsValid(event.target.elements);
-
-    if (allFieldsValid && this.props.onCreate) {
-      this.props
-        .onCreate({
-          name: this.state.name
-        })
-        .then(result => {
-          this.setState({
-            formState: "clean",
-            name: ""
-          });
-        });
-    }
-  }
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }
-  render() {
-    return (
-      <div className="todo-list-card card">
-        <div className="card-header">Create new todo list</div>
-        <div className="card-body">
-          <form
-            className={
-              "form" + (this.state.formState != "clean" ? " was-validated" : "")
-            }
-            onSubmit={e => this.onSubmit(e)}
-            noValidate
-          >
-            <div className="form-group">
-              <input
-                type="text"
-                value={this.state.name}
-                className="form-control"
-                name="name"
-                placeholder="New todo-list name ..."
-                onChange={e => this.handleInputChange(e)}
-                required
-              />
-              <div className="invalid-feedback">Please enter list name.</div>
-            </div>
-            <button className="btn btn-primary">Create</button>
-          </form>
+const BaseForm = ({
+  revalidation: {
+    form,
+    onChange,
+    updateState,
+    valid,
+    submitted,
+    errors,
+    onSubmit
+  },
+  onSubmit: submitCb
+}) => (
+  <div className="todo-list-card card">
+    <div className="card-header">Create new todo list</div>
+    <div className="card-body">
+      <form
+        className="form"
+        onSubmit={e => {
+          e.preventDefault();
+          onSubmit(({ form, valid }) =>
+            valid
+              ? submitCb(form)
+              : console.log("something went wrong!", errors)
+          );
+        }}
+        noValidate
+      >
+        <div className="form-group">
+          <input
+            type="text"
+            value={form.name}
+            className={`form-control ${
+              isValid(errors.name) || !submitted ? "" : "is-invalid"
+            }`}
+            name="name"
+            placeholder="New todo-list name ..."
+            onChange={e => onChange("name", getValue(e))}
+            required
+          />
+          <div className="invalid-feedback">{errors.name}</div>
         </div>
-      </div>
-    );
-  }
+        <button className="btn btn-primary">Create</button>
+      </form>
+    </div>
+  </div>
+);
+
+const validationRules = {
+  name: [[isNotEmpty, "Name should not be  empty."]]
+};
+
+const RevalidationForm = Revalidation(BaseForm);
+
+const initialState = {
+  name: ""
+};
+
+export function TodoListForm({ onCreate }) {
+  const [count, setCount] = useState(0);
+
+  const onSubmit = form =>
+    onCreate({
+      name: form.name
+    }).then(r => {
+      setCount(count + 1);
+    });
+
+  return (
+    <RevalidationForm
+      key={count}
+      rules={validationRules}
+      onSubmit={onSubmit}
+      initialState={initialState}
+      validateSingle={false}
+      validateOnChange={({ submitted }) => submitted}
+    />
+  );
 }
